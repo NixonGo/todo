30ae9a65017d30046f81bf72b1912c6f4911d614
NixonGo <EMAIL__GITHUB>
2026-05-11 15:31:37 +0500

    фÐsetup sqlite database

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"log"
+	"os"
+	"todo/pkg/db"
 	"todo/pkg/server"
 )
 
 func main() {
+	dbFile := "scheduler.db"
+	envDB := os.Getenv("TODO_DBFILE")
+
+	if envDB != "" {
+		dbFile = envDB
+	}
+
+	err := db.Init(dbFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Запуск сервера
 	server.Run()
+}
 }
